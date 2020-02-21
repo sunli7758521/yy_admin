a48aee3cdb69e169b3b5bb7b0119c7820f570389
XuanHang7758521 <sun18833424134@163.com>
2020-02-21 20:27:26 +0800

    上传实体类

---
+++ ruoyi-system/src/main/java/com/ruoyi/sysusersystem/domain/YHTZVo.java
@@ -0,0 +1,164 @@
+package com.ruoyi.sysusersystem.domain;
+
+/**
+ * @Author sunli
+ * @Date 2020/2/20
+ */
+public class YHTZVo {
+
+    private String planId;
+    private String title;
+    private String time;
+    private String deptName;
+    private String teamId;
+    private String content;
+
+    private String typeName;
+    private String rectificationPeople;
+    private String rectificationTime;
+    private String hiddenContent;
+    private String hiddenWz;
+    private String state;
+
+    private String hiddenJb;
+    private String jcDept;
+    private String jcJb;
+    private String remark;
+    private String longTime;
+
+    public String getLongTime() {
+        return longTime;
+    }
+
+    public void setLongTime(String longTime) {
+        this.longTime = longTime;
+    }
+
+    public String getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getRectificationPeople() {
+        return rectificationPeople;
+    }
+
+    public void setRectificationPeople(String rectificationPeople) {
+        this.rectificationPeople = rectificationPeople;
+    }
+
+    public String getRectificationTime() {
+        return rectificationTime;
+    }
+
+    public void setRectificationTime(String rectificationTime) {
+        this.rectificationTime = rectificationTime;
+    }
+
+    public String getHiddenContent() {
+        return hiddenContent;
+    }
+
+    public void setHiddenContent(String hiddenContent) {
+        this.hiddenContent = hiddenContent;
+    }
+
+    public String getHiddenWz() {
+        return hiddenWz;
+    }
+
+    public void setHiddenWz(String hiddenWz) {
+        this.hiddenWz = hiddenWz;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getHiddenJb() {
+        return hiddenJb;
+    }
+
+    public void setHiddenJb(String hiddenJb) {
+        this.hiddenJb = hiddenJb;
+    }
+
+    public String getJcDept() {
+        return jcDept;
+    }
+
+    public void setJcDept(String jcDept) {
+        this.jcDept = jcDept;
+    }
+
+    public String getJcJb() {
+        return jcJb;
+    }
+
+    public void setJcJb(String jcJb) {
+        this.jcJb = jcJb;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
